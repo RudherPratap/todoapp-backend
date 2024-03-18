f83cc7e4c810687963c5704667d8c11a8089143f
Rudher <rudrabhardwaj121201@gmail.com>
2024-03-18 21:10:48 +0530

    todo-backend

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,9 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/todoApp')
+mongoose.connect('mongodb+srv://rudrabhardwaj121201:rudr12%40dec@cluster0.ii19pvj.mongodb.net/todoApp')
 
-server.post('/add', (req, res) => {
+server.post('/api/todos', (req, res) => {
   const task = req.body.task;
   TodoModel.create({
     task: task
@@ -17,27 +17,27 @@ server.post('/add', (req, res) => {
     .catch(err => res.json(err))
 })
 
-server.get('/get', (req, res) => {
+server.get('/api/todos', (req, res) => {
   TodoModel.find().then(result => res.json(result))
-    .catch(err => res.json(err))
+    .catch(err => res.status(500).json(err))
 })
 
-server.put('/edit/:id', (req, res) => {
+server.put('/api/todos/:id/complete', (req, res) => {
   const { id } = req.params;
   // console.log(id);
   TodoModel.findByIdAndUpdate({ _id: id }, { completed: true })
     .then(result => res.json(result))
-    .catch(err => res.json(err))
+    .catch(err => res.status(500).json(err))
 })
 
-server.delete('/delete/:id', (req, res) => {
+server.delete('/api/todos/:id', (req, res) => {
   const { id } = req.params;
   TodoModel.findByIdAndDelete({ _id: id })
     .then(result => res.json(result))
     .catch(err => res.json(err))
 })
 
-server.put('/update/:id', (req, res) => {
+server.put('/api/todos/:id', (req, res) => {
   const { id } = req.params;
   const { text } = req.body;
 

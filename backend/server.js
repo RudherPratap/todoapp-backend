fcf3ea4f9d820a3d721921fe8964c1af5933b903
Rudher <rudrabhardwaj121201@gmail.com>
2024-03-18 12:13:47 +0530

    todo-backend

---
+++ backend/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const TodoModel = require('./models/todo')
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/todoApp')
+
+server.post('/add', (req, res) => {
+  const task = req.body.task;
+  TodoModel.create({
+    task: task
+  }).then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+server.get('/get', (req, res) => {
+  TodoModel.find().then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+server.put('/edit/:id', (req, res) => {
+  const { id } = req.params;
+  // console.log(id);
+  TodoModel.findByIdAndUpdate({ _id: id }, { completed: true })
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+server.delete('/delete/:id', (req, res) => {
+  const { id } = req.params;
+  TodoModel.findByIdAndDelete({ _id: id })
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+server.put('/update/:id', (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+
+  TodoModel.findByIdAndUpdate(id, { text }, { new: true }) // { new: true } returns the modified document
+    .then(updatedTodo => {
+      if (!updatedTodo) {
+        return res.status(404).json({ message: "Todo not found" });
+      }
+      res.json(updatedTodo);
+    })
+    .catch(err => res.status(500).json({ message: err.message }));
+});
+server.listen(3000, () => {
+  console.log("server is listening at 3000");
+})
+
